c167ea677479c7fc38645c59532ff4b98182b379
Logan Miller <46401738+Loganocm@users.noreply.github.com>
2026-01-06 09:00:38 -0500

    fixed connection issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: true,
   // AWS Elastic Beanstalk Production Backend (Port 8000)
-  apiUrl: 'http://parlor-prod-env.eba-giywc7uh.us-west-2.elasticbeanstalk.com',
+  apiUrl: '',
   googleMapsApiKey: 'AIzaSyBs37kBNIaTxZ3DRBe8pyTABKhEfY_YqFQ'
 };
